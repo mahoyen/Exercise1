04918c7f545e8a2db3f6cc2afaa62ae2f6775a42
Markus Ho-Yen <mahoyen@stud.ntnu.no>
2019-01-14 21:26:10 +0100

    coded c part

--- Part4/c/foo.c
+++ Part4/c/foo.c
@@ -6,17 +6,26 @@ int i = 0;
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
+    for (int count=0;count<1000000;count++) {
+	i +=1;
+    }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
+    for (int counting=0;counting<1000000;counting++) {
+	i -=1;
+    }
     return NULL;
 }
 
 
 int main(){
     // TODO: declare incrementingThread and decrementingThread (hint: google pthread_create)
+    pthread_t incrementingThread;
+    pthread_t decrementingThread;
+
     pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
     pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
     
